43d99c7d9b7631819235a5a0a889deb0444b6576
bobbypine <45605113+bobbypine@users.noreply.github.com>
2019-04-14 09:46:02 -0400

    Update Bids.py
    Added error handling for bad keywords.

--- Bids.py
+++ Bids.py
@@ -3,22 +3,27 @@ from stockxsdk import Stockx
 stockx = Stockx()
 
 while True:
-    item = input('What product?:')
-    if item == "":
-        print("Program Ended")
-        break
-    print()
-    product_id = stockx.get_first_product_id('{}'.format(item))
-    product = stockx.get_product(product_id)
-    size = input("What size?:")
-    print()
-    bids = stockx.get_bids(product_id)
-    print("{} Size {}".format(product.title, size))
-    print()
-    for bid in bids:
-        if bid.shoe_size == "{}".format(size):
-            print("Size: {} ${} {} {}(s)".format(bid.shoe_size, bid.order_price, bid.num_orders, bid.order_type))
-            print('--------------------------')
-    print()
+    try:
+        item = input('What product?:')
+        if item == "":
+            print("Program Ended")
+            break
+        print()
+        product_id = stockx.get_first_product_id('{}'.format(item))
+        product = stockx.get_product(product_id)
+        size = input("What size?:")
+        print()
+        bids = stockx.get_bids(product_id)
+        print("{} Size {}".format(product.title, size))
+        print()
+        for bid in bids:
+            if bid.shoe_size == "{}".format(size):
+                print("Size: {} ${} {} {}(s)".format(bid.shoe_size, bid.order_price, bid.num_orders, bid.order_type))
+                print('--------------------------')
+        print()
+    except:
+        print('No Results for {}'.format(item.title()))
+        print()
+
 
 
